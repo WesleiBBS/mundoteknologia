702b716de16bf831cce20deb6d9d029b7cfd69a0
Weslei BBS <wesleibbs@gmail.com>
2025-07-18 21:55:32 -0300

    Atualizações no CSS e layout

--- js/script.js
+++ js/script.js
@@ -99,11 +99,11 @@ document.addEventListener('DOMContentLoaded', function() {
             document.body.classList.toggle('light-mode');
             // Altera cor do botão conforme o tema
             if (document.body.classList.contains('light-mode')) {
-                this.textContent = '🌑 Modo Escuro';
+                this.textContent = '🌙Escuro';
                 this.style.background = '#e3e7ee';
                 this.style.color = '#0077b6';
             } else {
-                this.textContent = '🌙 Modo Claro';
+                this.textContent = '☀️Claro';
                 this.style.background = '#23272f';
                 this.style.color = '#00b4d8';
             }
